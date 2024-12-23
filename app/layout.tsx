fbc284cdf8f67874fdbd4b25c9de6bd4a7a056c1
Max <maxim@akimkin>
2024-12-24 00:32:05 +0100

    🟩 feat: initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,15 @@
+// #endregion --------------------------------------------------------------------------------------
+// #region ROOT LAYOUT
+// -----------------------------------------------------------------------------------------------*/
+type TRootLayoutProps = {
+  children: React.ReactNode;
+};
+
+const RootLayout: React.FC<TRootLayoutProps> = ({ children }) => (
+  <html lang="en">
+    <body>{children}</body>
+  </html>
+);
+RootLayout.displayName = "RootLayout";
+// #endregion --------------------------------------------------------------------------------------
+export default RootLayout;
