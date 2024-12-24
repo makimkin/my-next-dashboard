602dfd27bbc9010c5b03649d4cad4648fe810e1f
Max <maxim@akimkin>
2024-12-24 01:18:43 +0100

    🟩 feat: dashboard layout added

---
+++ app/dashboard/layout.tsx
@@ -0,0 +1,17 @@
+import { SideNav } from "@/app/ui/dashboard/sidenav";
+// #endregion --------------------------------------------------------------------------------------
+// #region LAYOUT COMPONENT
+// -----------------------------------------------------------------------------------------------*/
+type TLayoutProps = { children: React.ReactNode };
+
+const Layout: React.FC<TLayoutProps> = ({ children }) => (
+  <div className={"flex h-screen flex-col md:flex-row md:overflow-hidden"}>
+    <div className={"w-full flex-none md:w-64"}>
+      <SideNav />
+    </div>
+    <div className={"flex-grow p-6 md:overflow-y-auto md:p-12"}>{children}</div>
+  </div>
+);
+Layout.displayName = "LayoutComponent";
+// #endregion --------------------------------------------------------------------------------------
+export default Layout;
