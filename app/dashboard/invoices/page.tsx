6183d2a91f00e021e31acfdf399625b96983cf36
Max <maxim@akimkin>
2024-12-24 01:14:00 +0100

    🟩 feat: added dashboard & customers & invoices pages

---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,10 @@
+import clsx from "clsx";
+// #endregion --------------------------------------------------------------------------------------
+// #region PAGE COMPONENT
+// -----------------------------------------------------------------------------------------------*/
+type TPageProps = {};
+
+const Page: React.FC<TPageProps> = () => <div className={clsx("")}>Invoices page</div>;
+Page.displayName = "PageComponent";
+// #endregion --------------------------------------------------------------------------------------
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,12 @@
+import clsx from "clsx";
+// #endregion --------------------------------------------------------------------------------------
+// #region PAGE COMPONENT
+// -----------------------------------------------------------------------------------------------*/
+type TPageProps = {};
+
+const Page: React.FC<TPageProps> = () => (
+  <div className={clsx("")}>Dashboard page</div>
+);
+Page.displayName = "PageComponent";
+// #endregion --------------------------------------------------------------------------------------
+export default Page;
