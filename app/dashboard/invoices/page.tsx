88da378db029ae544f493537c294a1923cc73e87
Max <maxim@akimkin>
2024-12-24 13:44:07 +0100

    🟦 perf: reworked invoices table skeleton

--- app/dashboard/invoices/page.tsx
+++ app/dashboard/invoices/page.tsx
@@ -1,29 +1,46 @@
+import { Suspense } from "react";
+
 import { lusitana } from "@/app/ui/fonts";
 import { Search } from "@/app/ui/search";
 
 import { CreateInvoice } from "@/app/ui/invoices/buttons";
+import { Table } from "@/app/ui/invoices/table";
+
+import { InvoicesTableSkeleton } from "@/app/ui/skeletons";
+// import { Pagination } from "@/app/ui/invoices/pagination";
 // #endregion --------------------------------------------------------------------------------------
 // #region PAGE COMPONENT
 // -----------------------------------------------------------------------------------------------*/
-type TPageProps = {};
+type TPageProps = {
+  searchParams?: Promise<{
+    query?: string;
+    page?: string;
+  }>;
+};
 
-const Page: React.FC<TPageProps> = () => (
-  <div className="w-full">
-    <div className="flex w-full items-center justify-between">
-      <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+const Page: React.FC<TPageProps> = async ({ ...otherProps }) => {
+  const searchParams = await otherProps.searchParams;
+  const query = searchParams?.query || "";
+  const currentPage = Number(searchParams?.page) || 1;
+
+  return (
+    <div className="w-full">
+      <div className="flex w-full items-center justify-between">
+        <h1 className={`${lusitana.className} text-2xl`}>Invoices</h1>
+      </div>
+      <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
+        <Search placeholder="Search invoices..." />
+        <CreateInvoice />
+      </div>
+      <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
+        <Table query={query} currentPage={currentPage} />
+      </Suspense>
+      <div className="mt-5 flex w-full justify-center">
+        {/* <Pagination totalPages={totalPages} /> */}
+      </div>
     </div>
-    <div className="mt-4 flex items-center justify-between gap-2 md:mt-8">
-      <Search placeholder="Search invoices..." />
-      <CreateInvoice />
-    </div>
-    {/*  <Suspense key={query + currentPage} fallback={<InvoicesTableSkeleton />}>
-  <Table query={query} currentPage={currentPage} />
-</Suspense> */}
-    <div className="mt-5 flex w-full justify-center">
-      {/* <Pagination totalPages={totalPages} /> */}
-    </div>
-  </div>
-);
+  );
+};
 Page.displayName = "PageComponent";
 // #endregion --------------------------------------------------------------------------------------
 export default Page;
--- app/ui/skeletons/index.ts
+++ app/ui/skeletons/index.ts
@@ -1,3 +1,4 @@
+export { InvoicesTableSkeleton as InvoicesTableSkeleton } from "./invoices-table-skeleton";
 export { RevenueChartSkeleton as RevenueChartSkeleton } from "./revenue-chart-skeleton";
 export { InvoicesSkeleton as InvoicesSkeleton } from "./invoices-skeleton";
 export { CardsSkeleton as CardsSkeleton } from "./cards-skeleton";
