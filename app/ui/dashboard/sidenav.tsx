52d5ba702f280406f151ba027115d70fa191a7ed
Max <maxim@akimkin>
2024-12-24 21:41:23 +0100

    🟨 fix: using perform sign out action

--- app/ui/dashboard/sidenav.tsx
+++ app/ui/dashboard/sidenav.tsx
@@ -8,11 +8,10 @@ import AcmeLogo from "@/app/ui/acme-logo";
 import { NavLinks } from "@/app/ui/dashboard/nav-links/nav-links";
 
 import { performSignOut } from "@/app/lib/actions";
-import { signOut } from "@/auth";
 // #endregion --------------------------------------------------------------------------------------
 // #region SIDE NAV COMPONENT
 // -----------------------------------------------------------------------------------------------*/
-const SideNav: React.FC = () => (
+const SideNav: React.FC = async () => (
   <div className="flex h-full flex-col px-3 py-4 md:px-2">
     <Link
       className="mb-2 flex h-20 items-end justify-start rounded-md bg-blue-600 p-4 md:h-40"
@@ -25,12 +24,7 @@ const SideNav: React.FC = () => (
     <div className="flex grow flex-row justify-between space-x-2 md:flex-col md:space-x-0 md:space-y-2">
       <NavLinks />
       <div className="hidden h-auto w-full grow rounded-md bg-gray-50 md:block"></div>
-      <form
-        action={async () => {
-          "use server";
-          await signOut();
-        }}
-      >
+      <form action={performSignOut}>
         <button
           className={clsx(
             "flex h-[48px] w-full grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium",
